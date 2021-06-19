5e28ddce96e98769eda5dcfbc0d48248e73f990d
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-06-19 15:24:42 +0700

    v12

--- src/app/server.js
+++ src/app/server.js
@@ -10,7 +10,6 @@ app.use(express.static(__dirname + "/dist/client"));
 app.get('/*', function(req,res) {
    res.sendFile(path.join(__dirname+'/dist/client/index.html'));
 });
-console.log(__dirnamef);
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
